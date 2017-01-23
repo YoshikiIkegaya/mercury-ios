9ce33467c887c7f80b4d5d48c3a0a8279761068a
toco <tokoken1001@gmail.com>
2017-01-23 18:33:29 +0900

    Addded Room Screen

--- Mercury/RoomViewController.swift
+++ Mercury/RoomViewController.swift
@@ -9,21 +9,21 @@
 import UIKit
 
 class RoomViewController: UIViewController {
-  
+
   @IBOutlet weak var tableView: UITableView!
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
     setupTableViewUI()
   }
-  
+
   func setupTableViewUI() {
     let nib = UINib(nibName: "RoomTableViewCell", bundle: nil)
     self.tableView.register(nib, forCellReuseIdentifier: "Cell")
     self.tableView?.delegate = self
     self.tableView?.dataSource = self
   }
-  
+
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
@@ -40,10 +40,10 @@ extension RoomViewController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return 3
   }
-  
+
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! RoomTableViewCell
-    
+
     return cell
   }
 }
